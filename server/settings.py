cbbd62fe24bd77d0232def856c4e9e6b0c798cc4
Ari Bermeki <ari.bermeki@icloud.com>
2022-08-06 07:13:21 +0200

    2  commit

--- server/settings.py
+++ server/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = str(os.urandom(5000 * 80))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','karin-ari-project.Heroku.com']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
